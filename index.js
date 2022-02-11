e552199216537ecc33dbd35cc9eafa173a02afda
Firouzfar <firouzfar.sebastien@gmail.com>
2022-02-11 17:51:59 +0100

    Update the error / Modification de l'erreur

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-this.addEventListener("DOMContentLoaded", () =>{
-    const questions = document.querySelectorAll(".question")
-    questions.forEach((question) => question.addEventListener("click", () =>{
-  
-        if(question.parentNode.classList.contains("active")){
-            question.parentNode.classList.toggle("active")
-        }
-    else{
-    questions.forEach(question => question.parentNode.classList.remove("active"))
-    question.parentNode.classList.add("active")
-        }
-
-    }))
-}) 
+document.querySelectorAll(".question").forEach((question) => question.addEventListener("click", () => {
+    if(question.parentNode.classList.contains("active")){
+       question.parentNode.classList.toggle("active")
+    }else{
+        document.querySelectorAll(".question").forEach((question) => question.parentNode.classList.remove("active"))
+        question.parentNode.classList.add("active")
+    }
+}))
